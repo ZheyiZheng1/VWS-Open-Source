5065368caf26909e0e225a5b081246fd81da61b1
Fikayo Oyinloye <danieloyinloye@gmail.com>
2021-03-29 08:18:36 -0300

    pagination added to the show survey page and a few other design elements

--- src/resources/views/survey/show.blade.php
+++ src/resources/views/survey/show.blade.php
@@ -21,51 +21,52 @@
                 <a class="btn btn-dark" href="/surveys/questions/create?surveyId={{ $survey->id }}">Add New Question</a>
                 <a class="btn btn-dark" href="/surveys/participants/create?surveyId={{ $survey->id }}">Add a Participant</a>
             </div>
-            <div class="survey-create card mt-4" >
-                <div class="card-header">Questions
-                </div>
-                <div class="card-body">
-                    @foreach ($survey->questions as $question)
-                    <div class="">
-                        <div class="mt-4">
-                            <div class="d-flex flex-row justify-content-between">
-                                <h5 class="">{{$question->question}}</h5>
-                                    @if ($question->type === 'radio')
-                                        <p>Multiple Choice</p>
-                                    @endif
-                                    @if ($question->type === 'text')
-                                        <p>Text Field</p>
-                                    @endif
-                                    @if ($question->type === 'range')
-                                        <p>Likert Scale</p>
-                                    @endif
+            <div class="survey-create card mt-4">
+                <div class="card-header">Questions</div>
+                    <div class="card-body">
+                        @foreach ($survey->questions as $question)
+                            <div class="mt-2 card">
+                                <div class="card-header d-flex flex-row justify-content-between">
+                                    <h5>{{$question->question}}</h5>
+                                        @if ($question->type === 'radio')
+                                            <p>Multiple Choice</p>
+                                        @endif
+                                        @if ($question->type === 'text')
+                                            <p>Text Field</p>
+                                        @endif
+                                        @if ($question->type === 'range')
+                                            <p>Likert Scale</p>
+                                        @endif
+                                </div>
+                                <div class="card-body">
+                                    <ul class="list-group" style="list-style-type:none">
+                                        @foreach ($question->answers as $answer)
+                                        <li class="ml-2">
+                                            {{$answer->answerValue}}
+                                        </li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                                <div class="card-footer">
+                                    <form action="" method="post">
+                                        @csrf
+                                        <button class="btn btn-small btn-outline-danger" type="submit">Remove Question</button>
+                                    </form>
+                                </div>
                             </div>
-                            <ul class="list-group" style="list-style-type:none">
-                                @foreach ($question->answers as $answer)
-                                <li class="ml-2">
-                                    {{$answer->answerValue}}
-                                </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-header">Participants
-                </div>
-                <div class="card-body">
                     @endforeach
-                    @foreach ($surveyUserList as $user)
-                    <div>
-                        <div class="mt-4">
-                            <div class="d-flex flex-row justify-content-between">
-                            <h5>{{$user->name}}</h5>
-                            <!-- This example requires Tailwind CSS v2.0+ -->
-                            
-                        </div>
                     </div>
-                    @endforeach
+
+               </div>
+               <div class="survey-create card mt-2">
+                    <div class="card-header">Participants</div>
+                        <div class="card-body">
+                            @foreach ($surveyUserList as $user)
+                                <div>{{$user->name}}</div>
+                            @endforeach
+                            <div class="mt-2">{{ $surveyUserList->links() }}</div>
+                        </div>
                 </div>
-            </div>
         </section>
     </div>
 </body>
