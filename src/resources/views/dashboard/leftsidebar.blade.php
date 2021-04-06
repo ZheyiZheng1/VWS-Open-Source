9533c19518c164a6d134d058a11ab9c8a2dbc8e7
CodeItQuick <evanontario@hotmail.com>
2021-04-06 12:37:51 -0300

    made the dashboard more responsive

--- src/resources/views/dashboard/leftsidebar.blade.php
+++ src/resources/views/dashboard/leftsidebar.blade.php
@@ -2,33 +2,32 @@
 <section class="left-panel">
     <div class="vws-title"><h2>VWS</h2></div>
     <div class="wrapper">
-        <ul class="nav nav-pills nav-fill flex-column w-100" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-            <li class="nav-item">
-              <a class="nav-link {{ request()->routeIs('dashboard') ? 'active' : null }}" role="tablist" href="{{route('dashboard')}}">Dashboard</a>
+        <ul class="list-group" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <li class="list-group-item">
+              <a class="{{ request()->routeIs('dashboard') ? 'active' : null }}" role="tablist" href="{{route('dashboard')}}">Dashboard</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled"  href="#">Wellness Activity</a>
+            <li class="list-group-item">
+              <a class="disabled"  href="#">Wellness Activity</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link {{ request()->is('surveys*') ? 'active' : null }}" role="tablist" href="{{route('researchSurvey')}}">Survey</a>
+            <li class="list-group-item">
+              <a class="{{ request()->is('surveys*') ? 'active' : null }}" role="tablist" href="{{route('researchSurvey')}}">Survey</a>
             </li>
-            <li class="nav-item ">
-              <a class="nav-link {{ request()->is('dashboard/searchUserProfilePage*') ? 'active' : null }}" role="tablist" href="{{route('searchUsers')}}">Survey Viewer</a>
+            <li class="list-group-item ">
+              <a class="{{ request()->is('dashboard/searchUserProfilePage*') ? 'active' : null }}" role="tablist" href="{{route('searchUsers')}}">Survey Viewer</a>
             </li>
-            <li class="nav-item ">
-                <a class="nav-link disabled" href="#">Forums</a>
+            <li class="list-group-item ">
+                <a class="disabled" href="#">Forums</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#">Messages</a>
+              <li class="list-group-item">
+                <a class="disabled" href="#">Messages</a>
               </li>
               @auth
-              <li class="nav-item">
-                <a class="nav-link" href="#">{{ auth()->user()->name }}</a>
+              <li class="list-group-item">
+                <a class="href="#">{{ auth()->user()->name }}</a>
               </li>
 
-              <li class="nav-item">
-                <form action="{{ route('logout') }}" method="post" class="form-inline" style="
-                padding-left: 104px; width:100%">
+              <li class="list-group-item">
+                <form action="{{ route('logout') }}" method="post" class="form-inline" style="width:100%; display: flex; align-items: center; justify-content: center;">
                     @csrf
                         <button class="btn btn-outline-success" type="submit">Logout</button>
                     </form>
@@ -44,7 +43,7 @@
               </li>
               @endguest
 
-          </ul>
+        </ul>
     </div>
     <div class="upei-logo"><img src="{{URL::to('/images/UPEI_Logo.png')}}" style="height: 54px; width: 134px;"></img></div>
 </section>
